3df972fbc70519fecb9fc80d69074f1fa2ad8ee4
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-07-07 03:29:11 -0400

    setup express application

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+// creating varaible to store express methods
+const app = express()
+
+//middleware to use
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+module.exports = app;
+
